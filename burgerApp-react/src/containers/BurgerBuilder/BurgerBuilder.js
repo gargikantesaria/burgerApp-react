1aa83459e8163b917bd2ddeffe73d651047e1222
Gargi.kantesaria <Gargi.kantesaria@solutionanalysts.com>
2018-04-10 09:30:02 +0530

    initial commit

---
+++ burgerApp-react/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuidControls/BuildControls'
+
+class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {...}
+    // }
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 1,
+            meat: 1
+        }
+    }
+
+    addIngredientHandler = () => {}
+
+    removeIngredientHandler = () => {}
+
+    render () {
+        return (
+            <Aux>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls />
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
